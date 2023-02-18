510acb141f15a6b77ac17a697f84903ee52368d7
aminet2a <aminetbo@gmail.com>
2023-02-18 22:40:03 +0100

    fix validators assignment

--- src/main/java/patika/dev/definex/weaterApp/controller/WeatherController.java
+++ src/main/java/patika/dev/definex/weaterApp/controller/WeatherController.java
@@ -1,6 +1,7 @@
 package patika.dev.definex.weaterApp.controller;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -12,27 +13,35 @@ import patika.dev.definex.weaterApp.enums.BreakBy;
 import patika.dev.definex.weaterApp.enums.ChronoUnit;
 import patika.dev.definex.weaterApp.enums.Period;
 import patika.dev.definex.weaterApp.service.WeatherService;
+import patika.dev.definex.weaterApp.validator.model.ValidationError;
+import patika.dev.definex.weaterApp.validator.timesteps.TimeSteps;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static patika.dev.definex.weaterApp.config.constants.Constants.Path.*;
 
+// TODO add swagger doc
+
+@Slf4j
 @Validated
 @RestController
-@AllArgsConstructor
 @RequestMapping(WEATHER)
+@RequiredArgsConstructor
 public class WeatherController {
 
+    private final static int year = LocalDate.now().getYear();
     private final WeatherService mWeatherService;
 
+
     @GetMapping(FORECAST)
-    public ResponseEntity<?> getWeatherForecast(@RequestParam @NotNull String location,
-                                                @RequestParam(defaultValue = "5") @Positive Integer forecastDays,
-                                                @Positive @Min(0) Integer timesteps
+    public ResponseEntity<?> getWeatherForecast(@RequestParam(defaultValue = "5") @Positive Integer forecastDays,
+                                                @RequestParam @Size(min = 2, message = "Location must be provided") String location,
+                                                @TimeSteps(Values = {1, 12, 24}, message = "The Integer value is invalid, supported values are 1, 12 or 24.") Integer timesteps
     ) {
         return mWeatherService.getWeatherForecast(location, forecastDays, timesteps);
     }
@@ -40,24 +49,32 @@ public class WeatherController {
     @GetMapping(HISTORY)
     public ResponseEntity<?> getHistoricalWeather(
             Period period,
-            @RequestParam @NotNull String location,
-            @Positive @Min(0) Integer timestepsHours,
-            @Positive @Min(0) Integer timestepsMinutes,
+            @Positive Integer timestepsMinutes,
+            @RequestParam @Size(min = 2, message = "Location must be provided") String location,
             @PastOrPresent @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDateTime,
-            @PastOrPresent @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDateTime
+            @PastOrPresent @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDateTime,
+            @TimeSteps(Values = {1, 24}, message = "The Integer value is invalid, supported values are 1 or 24.") Integer timestepsHours
     ) {
         return mWeatherService.getHistoricalWeather(location, period, timestepsHours, timestepsMinutes, startDateTime, endDateTime);
     }
 
     @GetMapping(SUMMARY)
     public ResponseEntity<?> getHistoricalWeather(
-            @RequestParam @NotNull String location,
-            @Positive @Min(0) Integer timestepsHours,
-            @RequestParam @NotNull @Min(1970) Integer maxYear,
-            @RequestParam @NotNull @Min(1970) Integer minYear,
+            @RequestParam @Min(1970) Integer maxYear,
+            @RequestParam @Min(1970) Integer minYear,
             @RequestParam(defaultValue = "none") BreakBy breakBy,
-            @RequestParam(defaultValue = "weeks") ChronoUnit chronoUnit
+            @RequestParam(defaultValue = "weeks") ChronoUnit chronoUnit,
+            @RequestParam @Size(min = 2, message = "Location must be provided") String location,
+            @TimeSteps(Values = {1, 24}, message = "The Integer value is invalid, supported values are 1 or 24.") Integer timestepsHours
     ) {
+        if (minYear > maxYear || maxYear > year) {
+            return ResponseEntity.badRequest().body(
+                    ValidationError.builder()
+                            .field("maxYear")
+                            .message("maxYear must be between the minYear and the current calendar year inclusive")
+                            .build()
+            );
+        }
         return mWeatherService.getWeatherHistorySummary(location, chronoUnit, breakBy, timestepsHours, maxYear, minYear);
     }
 }
